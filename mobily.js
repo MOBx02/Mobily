b38544505505ab1b64a3237daf30ccf557f9fc07
MOBx02 <ahmadbaymik@gmail.com>
2022-11-27 16:57:02 +0200

    still not working

--- mobily.js
+++ mobily.js
@@ -4,18 +4,15 @@ const container = document.getElementsByClassName('main');
 
 
 
-// function start
+
     let myDiv = ducument.createElement("div");
     myDiv.className = "fir-image-figure mob border";
 
     let myImg = ducument.createElement("img");
     myImg.className = "fir-author-image img-box";
     myImg.setAttribute("src", "https://bit.ly/3UjIS8X");
-    myImg.setAttribute("alt","Profile");
+    myImg.setAttribute("alt", "Profile");
 
-    container.appendChild(myDiv)
-    myDiv.appenChild(myImg);
-// Append my img to div
 
     let divName = ducument.createElement("div");
     divName.className = "fig-author-figure-title img-username";
@@ -27,13 +24,14 @@ const container = document.getElementsByClassName('main');
     divTime.className = "fig-author-figure-title";
 // Divs for message 
 
-
     let myFigcaption= ducument.createElement("figcaption");
     myFigcaption.appenChild(divName, divMessage, divTime);
 
 
+    myDiv.appenChild(myImg);
+
     myDiv.appenChild(myFigcaption);
     // append fugcaption to main div
-// 
+    container.appendChild(myDiv);
 
 console.log(myDiv);
